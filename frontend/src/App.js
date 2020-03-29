c0d518dcb5211add4fe0497403e18d59177406b1
sauloq <sauloquinteiro@gmail.com>
2020-03-29 16:54:41 -0500

    front-end da aplicacao

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './global.css';
+import Routes from './routes';
+
+// JSX javascript e XML
+function App() {
+    return (
+    <Routes />
+  );
+}
+
+export default App;
